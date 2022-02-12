cea6acc363bc383b3eeba975a3ce4169e844aafc
aroolinks <aroolinks@gmail.com>
2022-02-12 18:35:20 +0500

    LOADING ASSETS / DIRECTORIES PATH

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 dotenv.config({ path: `config.env` });
@@ -9,6 +11,19 @@ const PORT = process.env.PORT || 8080;
 
 //log requests
 app.use(morgan('tiny'));
+
+//parese qrequest to bodyparser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine
+app.set('view engine', 'ejs');
+//if you create another folder in view folder
+//app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assest/css')));
+app.use('/js', express.static(path.resolve(__dirname, 'assest/js')));
+app.use('/img', express.static(path.resolve(__dirname, 'assest/img')));
 
 app.get('/', (req, res) => {
   res.send('Crud Application');
