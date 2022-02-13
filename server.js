ed6f078dfbbeccab9ec3d0620b80a7a671a564c4
aroolinks <aroolinks@gmail.com>
2022-02-13 18:37:04 +0500

    MONGO DB CONNECTTION

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
 const app = express();
+
 dotenv.config({ path: `config.env` });
 
 const PORT = process.env.PORT || 8080;
 
 //log requests
 app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
 
 //parese qrequest to bodyparser
 app.use(bodyparser.urlencoded({ extended: true }));
